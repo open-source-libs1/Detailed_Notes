5f4f5fbe3a67e6d85b6f44d26b21638cdab8716b
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-10-01 11:39:37 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,4 @@
-
-
-
-cd ~/Desktop/Projects/oct-1
-source .venv/bin/activate
-cd enrollment
-export PYTHONPATH="$(pwd)/src:$(pwd)/src/utils:$PYTHONPATH"
-
-# run only the helpers tests:
-coverage run -m unittest tests.utils.test_helpers -v
-coverage report -m
-
-
-
-
-coverage run -m unittest tests.test_helpers -v && coverage report -m
+cd ~/Desktop/Projects/oct-1 && source .venv/bin/activate && cd enrollment \
+&& export PYTHONPATH="$(pwd)/src:$(pwd)/src/utils" \
+&& coverage run -m unittest -v tests.utils.test_helpers \
+&& coverage report -m --include="$(pwd)/src/utils/helpers.py"
