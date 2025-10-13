af199e856b5aac33e60c7568d56292c240acee7e
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-10-13 12:45:57 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,6 @@
 
+import numpy as np
+
+for col in round_cols_rev:
+    mysql_pd[col] = mysql_pd[col].apply(lambda x: round(x, -1) if pd.notnull(x) else np.nan)
+    starrocks_pd[col] = starrocks_pd[col].apply(lambda x: round(x, -1) if pd.notnull(x) else np.nan)
