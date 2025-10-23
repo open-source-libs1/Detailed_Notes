93ac35f9a2aa36f3f1f3ea170d634735b6417ac5
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-10-23 07:06:50 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,15 +1,15 @@
-
 import pandas as pd
 import os
 
-# Create full export path
+output_name = f"Revenue_detail_"
 file_path = f"/Workspace/Shared/QA Test Automation/Star Rocks Migration/Validate_Table_Output/Latest-Sai/OCT-23/others/{output_name}{uw_req_id}_{time_stamp()}.xlsx"
 
 # Ensure directory exists
 os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-# Write to a single Excel sheet
-with pd.ExcelWriter(file_path, engine="openpyxl") as writer:
+# Write all data into one single Excel sheet
+with pd.ExcelWriter(file_path, engine="xlsxwriter") as writer:
     result_df.to_excel(writer, index=False, sheet_name="Results")
 
 print(f"✅ Exported all results to one sheet: {file_path}")
+
