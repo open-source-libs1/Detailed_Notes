48db4324d64ab8d092e4ac8f20dbce1b28cb9367
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-11-05 07:49:16 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,2 +1,11 @@
+# Rebate
 rebateOutput = spark.table("puma_qa.ce.rebate_summarization")
 rebateOutput.createOrReplaceTempView("Rebate")
+
+# Specialty
+specialtyOutput = spark.table("puma_qa.ce.specialty_summarization")
+specialtyOutput.createOrReplaceTempView("Specialty")
+
+# Revenue
+revenueOutput = spark.table("puma_qa.ce.revenue_summarization")
+revenueOutput.createOrReplaceTempView("Revenue")
