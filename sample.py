51246151852a9a0e1302e5a09df9a6676ec0677b
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-11-05 07:36:45 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,4 @@
-    # ... your other asserts ...
-    # 2) event_id sent to SQL is a string (adapter boundary)
-    self.assertIsInstance(params[-1], str)
-    self.assertEqual(params[-1], str(self.valid_evt.event_id))
-    # Optional: sanity that it’s a valid UUID string
-    self.assertEqual(UUID(params[-1]), self.valid_evt.event_id)
+
+
+rebateOutput = spark.read.format("delta").load('abfss://gold@sbmuscapfdatala.../RebateSummarization')
+rebateOutput.createOrReplaceTempView("Rebate")
