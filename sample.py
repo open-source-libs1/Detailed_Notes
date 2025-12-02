c275b22e938f02b79d389aeeedccee5e5687e4a6
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-12-02 10:34:07 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
+select distinct
+  upper(replace(bin_to_uuid(scenario_id),'-','')) as scenario_id,
+  lob_id,
+  upper(replace(bin_to_uuid(uw_req_id),'-','')) as uw_req_id
+from comp_engine_microservice.model_parameters mp
+where updated_at >= now() - interval 4 hour
+  and updated_at <  now() - interval 2 hour
+  and status = 'Completed';
 
