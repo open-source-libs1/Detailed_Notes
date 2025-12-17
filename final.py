f9ff3aee93efafb20c22fc84d9a04b87302ea97b
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-12-17 12:49:19 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,8 +1,3 @@
-# from your host, in the same folder as docker-compose.yml
-docker compose exec enrollment-writer bash   # or sh
-
-# now inside the container
-cd /app                                      # repo root (adjust if needed)
-pipenv run behave tests/component/features \
-  --format=json.pretty \
-  --outfile=./reports/cucumber.json
+# from the host, in the folder with docker-compose.yml
+docker compose exec enrollment-writer sh -lc \
+ 'cd /app && pipenv run behave tests/component/features --format=json.pretty --outfile=./reports/cucumber.json'
