3fb3d26bcf5eca5180379611aa268d883229cb33
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-12-17 12:53:52 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,5 +1,4 @@
 docker compose exec enrollment-writer sh -lc '
   cd /app \
-  && mkdir -p reports \
-  && behave tests/component/features --format=json.pretty --outfile=./reports/cucumber.json
+  && python -m behave tests/component/features --format=json.pretty --outfile=./reports/cucumber.json
 '
