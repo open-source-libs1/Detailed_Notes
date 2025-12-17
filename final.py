71aefbc3f828c2c50d09a7fb521342afc3ed9cfd
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-12-17 12:24:54 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,1 +1,30 @@
 
+
+# from repo root
+export ENDPOINT=http://localhost:4566
+
+ENR_URL=$(aws --endpoint-url="$ENDPOINT" sqs get-queue-url \
+  --queue-name sqsq2 \
+  --query 'QueueUrl' \
+  --output text)
+
+echo "Enrollment queue URL: $ENR_URL"
+
+aws --endpoint-url="$ENDPOINT" sqs send-message \
+  --queue-url "$ENR_URL" \
+  --message-body file://tests/local/enrollment-test.json
+
+
+
+------------------------------------------
+
+
+docker logs -f enrollment-writer
+
+
+
+-------------------------------------------
+
+
+
+
