9fda0a7ca1ac4c27d06a7556ce94ea4247f587cb
open-source-libs1 <sai.pythonengineer@gmail.com>
2025-12-31 04:39:34 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,20 +1,6 @@
-NO_RECORDS_MSG = "No records present for the Parent Request ID"
-
-if pre_dfm_output == NO_RECORDS_MSG:
-    msg = f"Issue in PRE DFM request: {pre_dfm_output}"
-    print(msg)
-    dbutils.notebook.exit(msg)
-
-elif post_dfm_output == NO_RECORDS_MSG:
-    msg = f"Issue in POST DFM request: {post_dfm_output}"
-    print(msg)
-    dbutils.notebook.exit(msg)
-
-else:
-    pre_df  = spark.read.format("delta").load(pre_dfm_output)
-    post_df = spark.read.format("delta").load(post_dfm_output)
-
-    # Only do this if the data is small enough to fit in driver memory
-    pre_pd  = pre_df.toPandas()
-    post_pd = post_df.toPandas()
-
+for name, df in [
+    ("rebate_conventional_df", rebate_conventional_df),
+    ("starrocks_rebate_df", starrocks_rebate_df),
+    ("compared_rebate_df", compared_rebate_df),
+]:
+    print(f"{name}: {type(df)}")
