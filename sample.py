e18bb5e9d43672b2f7e1229729079aa209ff37b0
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-01-16 15:22:55 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
+filter asv = "ASVCARDREFERRALS"
+filter logGroup = "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-east-1"
+
+statsby
+  sum(int64(cloudwatch_log ~ "Processing fulfillment record")) as processing_cnt,
+  sum(int64(cloudwatch_log ~ "Sending Onestream update"))      as onestream_cnt,
+  sum(int64(cloudwatch_log ~ "Processing fulfillment record"))
+    - sum(int64(cloudwatch_log ~ "Sending Onestream update"))  as failures_cnt
 
