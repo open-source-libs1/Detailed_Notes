2076c8785222b8e79df740a47b61660af6dd054f
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-01-20 15:46:28 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,13 @@
-
+filter asv = "ASVCARDREFERRALS"
+filter (
+  (
+    logGroup = "/aws/lambda/referrals-fulfillment-api-prod-us-east-1"
+    and cloudwatch_log ~ "(Failed to push to OneStream Kinesis stream|Failed to push records to Kinesis stream)"
+  )
+  or
+  (
+    logGroup = "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-east-1"
+    and cloudwatch_log ~ "(Failed to push records to Kinesis stream|while pushing record to Kinesis)"
+  )
+)
+statsby count()
