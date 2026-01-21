0f9d0ecd2768aac7c641dd7c7d846479bc48e7d4
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-01-21 07:32:51 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,16 @@
 
+filter asv = "ASVCARDREFERRALS"
+filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-(east-1|west-2)"
+statsby count()
+
+
+filter asv = "ASVCARDREFERRALS"
+filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-(east-1|west-2)"
+filter cloudwatch_log ~ "Sleeping"
+statsby count()
+
+
+filter asv = "ASVCARDREFERRALS"
+filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-(east-1|west-2)"
+extract_regex logGroup, /prod-us-(?<region>east-1|west-2)/
+statsby count(), region
