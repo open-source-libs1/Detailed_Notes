05d5e5a5653cc604e34a5b648aa966314b80d0bc
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-01-22 07:36:22 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,19 +7,21 @@ extract_regex cloudwatch_log, "program code\\s+'?(?<program_code>[A-Za-z0-9_-]+)
 filter not is_null(program_code)
 
 statsby ff_success_cnt: count(), group_by(program_code)
-sort ff_success_cnt desc
 
 
 
 //////////////////////////
 
 
-  filter asv = "ASVCARDREFERRALS"
+filter asv = "ASVCARDREFERRALS"
 filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-"
 
-// Only these exact patterns:
-make_col is_customer_relationship: int64(cloudwatch_log ~ "Other exception response for Customers Relationships" OR cloudwatch_log ~ "RequestException for Customers Relationships")
-make_col is_money_movement:        int64(cloudwatch_log ~ "error making money movement request")
+make_col is_customer_relationship:
+  int64(cloudwatch_log ~ "Other exception response for Customers Relationships")
+  + int64(cloudwatch_log ~ "RequestException for Customers Relationships")
+
+make_col is_money_movement:
+  int64(cloudwatch_log ~ "error making money movement request")
 
 filter (is_customer_relationship + is_money_movement) > 0
 
@@ -27,4 +29,3 @@ make_col error_category:
   if(is_customer_relationship > 0, "CustomerRelationship", "MoneyMovement")
 
 statsby failures: count(), group_by(error_category)
-sort failures desc
