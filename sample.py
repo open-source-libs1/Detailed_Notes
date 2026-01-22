79b678a6c029e7daf48b85e3cd657c1e569fce3e
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-01-22 07:32:14 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,30 @@
-
 filter asv = "ASVCARDREFERRALS"
-filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-(east-1|west-2)"
-statsby count()
+filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-"
+filter cloudwatch_log ~ "Successfully updated the customer status to FF"
+
+extract_regex cloudwatch_log, "program code\\s+'?(?<program_code>[A-Za-z0-9_-]+)'?"
+
+filter not is_null(program_code)
+
+statsby ff_success_cnt: count(), group_by(program_code)
+sort ff_success_cnt desc
 
 
-filter asv = "ASVCARDREFERRALS"
-filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-(east-1|west-2)"
-filter cloudwatch_log ~ "Sleeping"
-statsby count()
+
+//////////////////////////
 
 
-filter asv = "ASVCARDREFERRALS"
-filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-(east-1|west-2)"
-extract_regex logGroup, /prod-us-(?<region>east-1|west-2)/
-statsby count(), region
+  filter asv = "ASVCARDREFERRALS"
+filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-"
+
+// Only these exact patterns:
+make_col is_customer_relationship: int64(cloudwatch_log ~ "Other exception response for Customers Relationships" OR cloudwatch_log ~ "RequestException for Customers Relationships")
+make_col is_money_movement:        int64(cloudwatch_log ~ "error making money movement request")
+
+filter (is_customer_relationship + is_money_movement) > 0
+
+make_col error_category:
+  if(is_customer_relationship > 0, "CustomerRelationship", "MoneyMovement")
+
+statsby failures: count(), group_by(error_category)
+sort failures desc
