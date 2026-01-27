e620f03d92237cd23a2201b7c3234fe8edc655b1
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-01-27 07:55:30 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,10 +1,5 @@
-filter asv = "ASVCARDREFERRALS"
-filter logGroup ~ "/aws/ecs/fargate/BACARDREFERRALS/fulfillment-incentive-processor-prod-us-"
-filter cloudwatch_log ~ "Successfully updated the customer status to FF"
-
-extract_regex cloudwatch_log, /program code\s+'?(?<program_code>[A-Za-z0-9_-]+)'?/
-
-filter not is_null(program_code)
-
-statsby ff_success_cnt: count()
-group_by(program_code)
+pipenv run pytest --maxfail=1 --disable-warnings -q \
+  --cov=app \
+  --cov-report=term-missing \
+  --cov-report=html \
+  --cov-report=xml
