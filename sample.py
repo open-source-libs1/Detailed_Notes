3eebb3065a8c13bf5a51a89c56da7bbef79a7797
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-14 17:45:09 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,14 +1,1 @@
-SELECT
-  REPLACE(UPPER(BIN_TO_UUID(uw_req_id)), '-', '') AS uw_req_id,
-  REPLACE(UPPER(BIN_TO_UUID(scenario_id)), '-', '') AS scenario_id,
-  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS mysql_seconds,
-  updated_at
-FROM comp_engine_microservice.model_parameters
-WHERE
-  status = 'Completed'
-  AND updated_at >= $__timeFrom()
-  AND updated_at <= $__timeTo()
-  AND started_at IS NOT NULL
-  AND updated_at IS NOT NULL
-ORDER BY updated_at DESC
-LIMIT 200;
+
