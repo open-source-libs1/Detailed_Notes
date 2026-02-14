9a6749fe41cc510435bcf4043aa1eb638cfb2999
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-14 10:21:18 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,46 +1,8 @@
 SELECT
-  $__timeGroup(updated_at, '1m') AS time,
-  AVG(TIMESTAMPDIFF(SECOND, started_at, updated_at)) AS value,
-  'StarRocks' AS metric
-FROM comp_engine_microservice_qa.model_parameters
-WHERE
-  status = 'Completed'
-  AND updated_at >= $__timeFrom()
-  AND updated_at <= $__timeTo()
-  AND started_at IS NOT NULL
-  AND updated_at IS NOT NULL
-GROUP BY 1
-ORDER BY 1;
-
-
-
-/////////////
-
-
-SELECT
-  $__timeGroup(updated_at, '1m') AS time,
-  AVG(TIMESTAMPDIFF(SECOND, started_at, updated_at)) AS value,
-  'MySQL' AS metric
-FROM comp_engine_microservice.model_parameters
-WHERE
-  status = 'Completed'
-  AND updated_at >= $__timeFrom()
-  AND updated_at <= $__timeTo()
-  AND started_at IS NOT NULL
-  AND updated_at IS NOT NULL
-GROUP BY 1
-ORDER BY 1;
-
-
-
-//////////////////
-
-SELECT
+  updated_at AS time,
+  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS starrocks_seconds,
   uw_req_id,
-  scenario_id,
-  started_at,
-  updated_at,
-  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS duration_seconds
+  scenario_id
 FROM comp_engine_microservice_qa.model_parameters
 WHERE
   status = 'Completed'
@@ -48,8 +10,7 @@ WHERE
   AND updated_at <= $__timeTo()
   AND started_at IS NOT NULL
   AND updated_at IS NOT NULL
-ORDER BY updated_at DESC
-LIMIT 200;
+ORDER BY updated_at;
 
 
 
@@ -57,11 +18,10 @@ LIMIT 200;
 
 
 SELECT
+  updated_at AS time,
+  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS mysql_seconds,
   BIN_TO_UUID(uw_req_id) AS uw_req_id,
-  BIN_TO_UUID(scenario_id) AS scenario_id,
-  started_at,
-  updated_at,
-  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS duration_seconds
+  BIN_TO_UUID(scenario_id) AS scenario_id
 FROM comp_engine_microservice.model_parameters
 WHERE
   status = 'Completed'
@@ -69,7 +29,4 @@ WHERE
   AND updated_at <= $__timeTo()
   AND started_at IS NOT NULL
   AND updated_at IS NOT NULL
-ORDER BY updated_at DESC
-LIMIT 200;
-
-
+ORDER BY updated_at;
