2dd0bd52e8d24fb7fa0481b1213f1d5479504ddb
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-14 10:59:12 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,26 +1,6 @@
 SELECT
-  uw_req_id,
-  scenario_id,
-  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS starrocks_seconds,
-  updated_at
-FROM comp_engine_microservice_qa.model_parameters
-WHERE
-  status = 'Completed'
-  AND updated_at >= $__timeFrom()
-  AND updated_at <= $__timeTo()
-  AND started_at IS NOT NULL
-  AND updated_at IS NOT NULL
-ORDER BY updated_at DESC
-LIMIT 200;
-
-
-
-////////////
-
-
-SELECT
-  BIN_TO_UUID(uw_req_id) AS uw_req_id,
-  BIN_TO_UUID(scenario_id) AS scenario_id,
+  REPLACE(UPPER(BIN_TO_UUID(uw_req_id)), '-', '') AS uw_req_id,
+  REPLACE(UPPER(BIN_TO_UUID(scenario_id)), '-', '') AS scenario_id,
   TIMESTAMPDIFF(SECOND, started_at, updated_at) AS mysql_seconds,
   updated_at
 FROM comp_engine_microservice.model_parameters
