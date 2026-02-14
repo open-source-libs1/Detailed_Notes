fd099d71f8aaa1ad66e3913a6005e81650ef48d9
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-14 10:36:27 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,32 +1,32 @@
 SELECT
-  updated_at AS time,
-  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS starrocks_seconds,
   uw_req_id,
-  scenario_id
+  scenario_id,
+  started_at,
+  updated_at,
+  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS duration_seconds
 FROM comp_engine_microservice_qa.model_parameters
 WHERE
   status = 'Completed'
   AND updated_at >= $__timeFrom()
   AND updated_at <= $__timeTo()
-  AND started_at IS NOT NULL
-  AND updated_at IS NOT NULL
-ORDER BY updated_at;
+ORDER BY updated_at DESC
+LIMIT 200;
 
 
 
-//////////////
+/////////////
 
 
 SELECT
-  updated_at AS time,
-  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS mysql_seconds,
   BIN_TO_UUID(uw_req_id) AS uw_req_id,
-  BIN_TO_UUID(scenario_id) AS scenario_id
+  BIN_TO_UUID(scenario_id) AS scenario_id,
+  started_at,
+  updated_at,
+  TIMESTAMPDIFF(SECOND, started_at, updated_at) AS duration_seconds
 FROM comp_engine_microservice.model_parameters
 WHERE
   status = 'Completed'
   AND updated_at >= $__timeFrom()
   AND updated_at <= $__timeTo()
-  AND started_at IS NOT NULL
-  AND updated_at IS NOT NULL
-ORDER BY updated_at;
+ORDER BY updated_at DESC
+LIMIT 200;
