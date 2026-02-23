884d68666e945369bea125ba7918668897938067
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-23 07:25:07 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,20 @@
-python3.12 -m pip install -v --no-deps --dry-run pipenv
+export PIP_INDEX_URL="https://artifactory.cloud.capitalone.com/artifactory/api/pypi/pypi-internalfacing/simple"
+export PIP_TRUSTED_HOST="artifactory.cloud.capitalone.com"
+unset PIP_EXTRA_INDEX_URL
+
+
+pipx uninstall pipenv 2>/dev/null || true
+pipx install --python /opt/homebrew/bin/python3.12 pipenv -v
+
+
+pipenv --version
+
+
+cd /Users/rcw839/Desktop/Projects/2026/FEB/bank-offers-detail-synchronization
+
+pipenv --rm 2>/dev/null || true
+
+PIPENV_PYTHON=/opt/homebrew/bin/python3.12 pipenv install --dev
+PIPENV_PYTHON=/opt/homebrew/bin/python3.12 pipenv lock --clear
+
+pipenv run python --version
