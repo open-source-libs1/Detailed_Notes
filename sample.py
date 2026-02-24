b1c3d5136a0001751ac9b8a16bb4774c488f29d7
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-24 07:04:44 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,4 @@
-auth-type=legacy
-registry=https://artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/
-always-auth=true
-
-export DOCKER_BUILDKIT=1
-printf "%s" "$NPM_TOKEN" | docker build \
-  --secret id=npm_token,src=/dev/stdin \
-  -t f1000-docker .
-
-
+# Create npm auth config using BuildKit secret (token is NOT stored in image layers)
 RUN --mount=type=secret,id=npm_token \
-    sh -c 'printf "auth-type=legacy\nregistry=https://artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/\nalways-auth=true\n//artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/:_authToken=%s\n" "$(cat /run/secrets/npm_token)" > /root/.npmrc'
+    sh -c 'cat .npmrc.template > /root/.npmrc && \
+           printf "\n//artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/:_authToken=%s\n" "$(cat /run/secrets/npm_token)" >> /root/.npmrc'
