c99824e7ad185da53b46626414e621786682c0b1
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-24 07:05:04 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,3 +2,10 @@
 RUN --mount=type=secret,id=npm_token \
     sh -c 'cat .npmrc.template > /root/.npmrc && \
            printf "\n//artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/:_authToken=%s\n" "$(cat /run/secrets/npm_token)" >> /root/.npmrc'
+
+
+
+export DOCKER_BUILDKIT=1
+printf "%s" "$NPM_TOKEN" | docker build --progress=plain \
+  --secret id=npm_token,src=/dev/stdin \
+  -t f1000-docker .
