6019507287eb9e60039cb2ba0c4087ace79289c1
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-24 06:56:23 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,32 +1,12 @@
-DOCKER_BUILDKIT=1 docker build --progress=plain \
-  --build-arg app_env=local \
-  --build-arg oneingest_application=BAOFFERFULFILLMENTENGINE \
-  --build-arg oneingest_schema=deposit_market_decide_incentive_fulfillment_status_updated_v2 \
-  --build-arg devexchange_url=https://api-it.cloud.capitalone.com \
-  --build-arg client_id=3ba05106a04b4faf8af63d1a8c3c58f8 \
-  --build-arg http_proxy=${HTTP_PROXY} \
-  --build-arg https_proxy=${HTTPS_PROXY} \
-  --build-arg no_proxy=${NO_PROXY} \
-  --build-arg DX_CLIENT_SECRET=${CLIENT_SECRET} \
+auth-type=legacy
+registry=https://artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/
+always-auth=true
+
+export DOCKER_BUILDKIT=1
+printf "%s" "$NPM_TOKEN" | docker build \
+  --secret id=npm_token,src=/dev/stdin \
   -t f1000-docker .
 
 
-    ///////////////////
-
-
-docker run --rm -it -v "$HOME:/host" artifactory-edge-staging.cloud.capitalone.com/baenterprisesharedimages-docker/languages/node:22-ubuntu24.04-202602231446 bash
-
-
-npm config set registry https://artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/
-npm login --registry=https://artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/
-cp /root/.npmrc /host/.npmrc-f1000
-exit
-
-
-RUN --mount=type=secret,id=npmrc,target=/root/.npmrc npm install
-RUN --mount=type=secret,id=npmrc,target=/root/.npmrc npm install --production
-
-
-export DOCKER_BUILDKIT=1
-docker build --progress=plain --secret id=npmrc,src=$HOME/.npmrc-f1000 -t f1000-docker .
-
+RUN --mount=type=secret,id=npm_token \
+    sh -c 'printf "auth-type=legacy\nregistry=https://artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/\nalways-auth=true\n//artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/:_authToken=%s\n" "$(cat /run/secrets/npm_token)" > /root/.npmrc'
