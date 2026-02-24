70b520ecf2a8f16aff1a07b62cf46d0120948bf5
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-24 07:23:44 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,12 @@
-# Create npm auth config using BuildKit secret (token is NOT stored in image layers)
-RUN --mount=type=secret,id=npm_token \
-    sh -c 'cat .npmrc.template > /root/.npmrc && \
-           printf "\n//artifactory.cloud.capitalone.com/artifactory/api/npm/npm-internalfacing/:_authToken=%s\n" "$(cat /run/secrets/npm_token)" >> /root/.npmrc'
-
-
-
 export DOCKER_BUILDKIT=1
-printf "%s" "$NPM_TOKEN" | docker build --progress=plain \
+
+printf "%s" "$NPM_TOKEN" | docker build \
   --secret id=npm_token,src=/dev/stdin \
+  --build-arg app_env=local \
+  --build-arg oneingest_application=BAOFFERFULFILLMENTENGINE \
+  --build-arg oneingest_schema=deposit_market_decide_incentive_fulfillment_status_updated_v2 \
+  --build-arg devexchange_url=https://api-it.cloud.capitalone.com \
+  --build-arg client_id=3ba05106a04b4faf8af63d1a8c3c58f8 \
+  --build-arg http_proxy=${HTTP_PROXY} \
+  --build-arg DX_CLIENT_SECRET=${CLIENT_SECRET} \
   -t f1000-docker .
