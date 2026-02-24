620901636bd7bd8da4268deb0298d7062875fa8c
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-24 06:39:05 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,14 @@
-pipenv run python -m pip install -v "aiohappyeyeballs==2.6.1" 2>&1 | tail -n 80
+WORKDIR /src
+
+COPY . /src
+
+RUN echo "current directory: $PWD"
+RUN ls
+
+# >>> ADD THESE TWO DEBUG LINES RIGHT HERE <<<
+RUN node -v && npm -v
+RUN npm config get registry && npm config list
+
+# existing lines (leave as-is)
+RUN npm install
+RUN echo "yes" | npm install --production
