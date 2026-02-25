f73c16187d4f6cd2688fd0c5a909250684dbcea1
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-24 18:18:26 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,1 @@
-export DOCKER_BUILDKIT=1
 
-printf "%s" "$NPM_TOKEN" | docker build \
-  --secret id=npm_token,src=/dev/stdin \
-  --build-arg app_env=local \
-  --build-arg oneingest_application=BAOFFERFULFILLMENTENGINE \
-  --build-arg oneingest_schema=deposit_market_decide_incentive_fulfillment_status_updated_v2 \
-  --build-arg devexchange_url=https://api-it.cloud.capitalone.com \
-  --build-arg client_id=3ba05106a04b4faf8af63d1a8c3c58f8 \
-  --build-arg http_proxy=${HTTP_PROXY} \
-  --build-arg DX_CLIENT_SECRET=${CLIENT_SECRET} \
-  -t f1000-docker .
-
-
-
-    docker run -p 3001:3001 --network container_default f1000-docker
