ba463bfa60f12df754b6fc5f27d739756bdb744e
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-02-25 07:48:04 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,3 @@
 
+echo "len=$(printf %s "$NPM_TOKEN" | wc -c | tr -d ' ')"
+printf "%s" "$NPM_TOKEN" | docker build --no-cache --progress=plain --secret id=npm_token,src=/dev/stdin -t f1000-docker .
