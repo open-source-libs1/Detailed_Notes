1b841247d93cd912d3955290e11e82db2535ec4e
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-03-13 08:45:42 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,4 @@
 
+RUN ["npm", "install", "webpack", "webpack-cli", "mini-css-extract-plugin", "--legacy-peer-deps", "--no-audit", "--no-fund"]
+RUN ["npm", "install", "--legacy-peer-deps", "--no-audit", "--no-fund"]
+RUN ["npm", "run", "build"]
