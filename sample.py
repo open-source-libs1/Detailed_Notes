0b0d6f0f5fbd1cc86173acf22153614bbb620d91
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-03-18 07:34:44 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,11 @@
-# final runtime stage
 WORKDIR /src
+
 COPY --from=builder --chown=1000:1000 /src /src
 COPY --chown=1000:1000 sm_secrets.yaml /app/sm_secrets.yaml
+COPY --chown=1000:1000 sm_secrets.yaml /src/sm_secrets.yaml
 
 USER 1000
 
 EXPOSE ${APP_PORT}
-CMD ["node", "app/index.js"]
+
+CMD ["node", "/src/app/index.js"]
