a1410a1a49fdd41d7bb843a49ee09c523ae19e6f
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-03-25 10:03:06 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "offer-details-synchronization-consumer"
 filter cloudwatch_log ~ "RequestException for Workfront request after retry"
-extract_regex cloudwatch_log, /message\\\\":\\\\"(?P<wf_error>[^\\\\"]+)/
+extract_regex cloudwatch_log, /Error: \{\\"error\\":\{\\"message\\":\\"(?P<wf_error>[^"]+)/
 statsby err_count: count(1), group_by(wf_error)
-sort err_count desc
+sort err_count
