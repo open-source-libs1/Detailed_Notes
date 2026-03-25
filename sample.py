d3c3e2aedabf986bab03dd9a6c047fc8bb354fde
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-03-25 09:53:56 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,28 +1,18 @@
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "offer-details-synchronization-consumer"
 filter cloudwatch_log ~ "RequestException for Workfront request after retry"
-make_col wf_error: extract_regex(cloudwatch_log, 'message\\\\":\\\\"([^\\\\"]+)')
-statsby count: count(), group_by(wf_error)
+extract_regex cloudwatch_log, /message\\\\":\\\\"(?P<wf_error>[^\\\\"]+)/
+statsby count: count() by wf_error
 sort -count
 
 
+--------------
 
------------------
-
-
-filter asv = "ASVOFFERFULFILLMENTENGINE"
-filter logGroup ~ "offer-details-synchronization-consumer"
-filter cloudwatch_log ~ "RequestException for Workfront request after retry"
-make_col wf_error: extract_regex(cloudwatch_log, 'message\\\\":\\\\"([^\\\\"]+)')
-statsby count: count() by wf_error
-
-
-------------------
 
 
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "offer-details-synchronization-consumer"
 filter cloudwatch_log ~ "RequestException for Workfront request after retry"
-make_col wf_error: regexp_extract(cloudwatch_log, 'message\\\\":\\\\"([^\\\\"]+)', 1)
+extract_regex cloudwatch_log, /Error: \{\\?"error\\?":\{\\?"message\\?":\\?"(?P<wf_error>[^\\"]+)/
 statsby count: count() by wf_error
 sort -count
