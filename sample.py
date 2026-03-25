2f3adb764574b757a19a80c55e580634f26a94ab
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-03-25 09:45:31 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,5 @@
-WORKDIR /src
-
-COPY --from=builder --chown=1000:1000 /src /src
-COPY --chown=1000:1000 sm_secrets.yaml /app/sm_secrets.yaml
-COPY --chown=1000:1000 sm_secrets.yaml /src/sm_secrets.yaml
-
-USER 1000
-
-EXPOSE ${APP_PORT}
-
-CMD ["node", "/src/app/index.js"]
+filter asv = "ASVOFFERFULFILLMENTENGINE"
+filter logGroup ~ "offer-details-synchronization-consumer"
+filter cloudwatch_log ~ "RequestException for Workfront request after retry"
+parse regex field=cloudwatch_log 'Error: .*?message\\\\":\\\\"(?<wf_error>[^\\\\"]+)'
+statsby count() by wf_error
