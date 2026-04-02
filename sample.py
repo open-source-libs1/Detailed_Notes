0f659866381ec10bed917d2cf8cb1bfd75cd2d74
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-02 08:23:23 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,5 @@
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "offer-details-synchronization-consumer"
-extract_regex arn, /arn:aws:logs:(?P<active_region>us-(?:east|west)-\d):/
-statsby log_count: count(1), group_by(active_region)
+statsby log_count: count(1), group_by(region)
 sort -log_count
 limit 1
