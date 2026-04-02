c9a7eb724d844e7a0e275c004f92bdb9ab7534a4
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-02 08:20:55 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,15 +1,6 @@
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "offer-details-synchronization-consumer"
-filter cloudwatch_log ~ "Got Validation Errors"
-extract_regex cloudwatch_log, /"msg":\s*"(?P<validation_error>[^"]+)"/
-statsby err_count: count(1), group_by(validation_error)
-sort err_count
-
-
-
-filter asv = "ASVOFFERFULFILLMENTENGINE"
-filter logGroup ~ "offer-details-synchronization-consumer"
-filter cloudwatch_log ~ "Got Validation Errors"
-extract_regex cloudwatch_log, /'msg':\s*'(?P<validation_error>[^']+)'/
-statsby err_count: count(1), group_by(validation_error)
-sort err_count
+extract_regex arn, /arn:aws:logs:(?P<active_region>us-(?:east|west)-\d):/
+statsby log_count: count(1), group_by(active_region)
+sort -log_count
+limit 1
