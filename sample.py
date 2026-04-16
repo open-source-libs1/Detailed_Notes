80c50b364d6056d12bc64f5401f55aaaa79ba2e2
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-16 17:01:02 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,1 @@
-filter asv = "ASVOFFERFULFILLMENTENGINE"
-filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
-filter cloudwatch_log ~ "\"message\":\"processing /payout/offers request\""
-sort timestamp desc
+
