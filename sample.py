bd3c0019f64c8aa7ed5d2bbcbc3189bb15ed5b11
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-20 07:52:52 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,19 @@
 
+
+
+filter asv = "ASVOFFERFULFILLMENTENGINE"
+filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
+filter cloudwatch_log ~ "\"message\":\"processing /payout/offers request\""
+filter not (cloudwatch_log ~ "attempting to send Payload")
+filter not (cloudwatch_log ~ "account_number_tokenized")
+sort timestamp desc
+
+
+
+filter asv = "ASVOFFERFULFILLMENTENGINE"
+filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
+filter cloudwatch_log ~ "processing /payout/offers request"
+filter not (cloudwatch_log ~ "attempting to send Payload")
+filter not (cloudwatch_log ~ "On")
+filter not (cloudwatch_log ~ "account_number_tokenized")
+sort timestamp desc
