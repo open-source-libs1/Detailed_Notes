f62d432be31d36999324e74847804be53be1a3b0
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-20 09:45:52 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,25 @@
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
-filter cloudwatch_log ~ "{\"level\":\"info\",\"message\":\"processing /payout/offers request\""
-statsby count()
+filter cloudwatch_log ~ "updated account to updateItemParam:"
+
+parse_regex cloudwatch_log /\\\"promo_code\\\":\\\"(?<promo_code>[^\\\"]+)\\\"/
+parse_regex cloudwatch_log /\\\"reward_amount\\\":\\\"(?<reward_amount>[0-9]+)\\\"/
+
+make_col reward_amount_num = int64(reward_amount)
+
+statsby sum(reward_amount_num), promo_code, timechart(timestamp, 1h)
+
+
+/////////////////////
 
 
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
-filter cloudwatch_log ~ "{\"level\":\"info\",\"message\":\"completed /payout/offers request\""
-statsby count()
+filter cloudwatch_log ~ "updated account to updateItemParam:"
 
+parse_regex cloudwatch_log /\\\"promo_code\\\":\\\"(?<promo_code>[^\\\"]+)\\\"/
+parse_regex cloudwatch_log /\\\"reward_amount\\\":\\\"(?<reward_amount>[0-9]+)\\\"/
 
-filter asv = "ASVOFFERFULFILLMENTENGINE"
-filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
-filter cloudwatch_log ~ "{\"level\":\"info\",\"message\":\"completed /payout/offers request\""
-sort timestamp desc
+make_col reward_amount_num = int64(reward_amount)
+
+statsby sum(reward_amount_num), promo_code, timestamp:1h                                                      
