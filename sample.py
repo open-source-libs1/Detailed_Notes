86aa3d58e4fddc89b7f9062648b144d93a3eadc9
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-20 09:52:07 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,25 +1,8 @@
 filter asv = "ASVOFFERFULFILLMENTENGINE"
 filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
-filter cloudwatch_log ~ "updated account to updateItemParam:"
+filter cloudwatch_log ~ "{\"level\":\"info\",\"message\":\"updated account to updateItemParam:"
 
-parse_regex cloudwatch_log /\\\"promo_code\\\":\\\"(?<promo_code>[^\\\"]+)\\\"/
-parse_regex cloudwatch_log /\\\"reward_amount\\\":\\\"(?<reward_amount>[0-9]+)\\\"/
+extract_regex cloudwatch_log, /promo_code\\":\\"(?P<promocode>[^\\"]+)/
+extract_regex cloudwatch_log, /reward_amount\\":\\"(?P<rewardamount::int64>[0-9]+)/
 
-make_col reward_amount_num = int64(reward_amount)
-
-statsby sum(reward_amount_num), promo_code, timechart(timestamp, 1h)
-
-
-/////////////////////
-
-
-filter asv = "ASVOFFERFULFILLMENTENGINE"
-filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prodf1000-docker/"
-filter cloudwatch_log ~ "updated account to updateItemParam:"
-
-parse_regex cloudwatch_log /\\\"promo_code\\\":\\\"(?<promo_code>[^\\\"]+)\\\"/
-parse_regex cloudwatch_log /\\\"reward_amount\\\":\\\"(?<reward_amount>[0-9]+)\\\"/
-
-make_col reward_amount_num = int64(reward_amount)
-
-statsby sum(reward_amount_num), promo_code, timestamp:1h                                                      
+timechart 1h, totalreward:sum(rewardamount), group_by(promocode)
