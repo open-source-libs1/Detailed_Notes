252497445372b0a9a0c68fa773a43573cb82bda5
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-04-29 12:05:12 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,41 @@
-https://pcrefresh.powerappsportals.com/cvsmacnewhire/
+git config --global user.name "SaiSrikar-Ravipati_cvsh"
+git config --global user.email "SaiSrikar.Ravipati@CVSHealth.com"
+
+ls -al ~/.ssh
+
+
+ssh-keygen -t ed25519 -C "SaiSrikar.Ravipati@CVSHealth.com" -f ~/.ssh/id_ed25519_cvs
+
+
+eval "$(ssh-agent -s)"
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519_cvs
+
+---
+ssh-add ~/.ssh/id_ed25519_cvs
+
+cat >> ~/.ssh/config <<'EOF'
+
+Host github.com
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_ed25519_cvs
+  AddKeysToAgent yes
+  UseKeychain yes
+EOF
+
+chmod 600 ~/.ssh/config
+
+
+pbcopy < ~/.ssh/id_ed25519_cvs.pub
+
+
+ssh -T git@github.com
+
+
+cd ~/april
+git clone -b sai-test git@github.com:cvs-health-source-code/pfme.git
+
+
+---------------------------------------
+
+
