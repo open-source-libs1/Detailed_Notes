a2b0c4e9c7232a9fe72e250f0b150cffd4d846e4
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-05-12 15:30:16 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,1 @@
-This file is only for running the Pact contract test locally.
 
-It starts the local Pact mock server and generates the pact file used later by PactFlow in the pipeline. `CURRENT_DIR` just makes the file paths work no matter where pytest is run from.
