166456d6e9cfab9326541e7e6e88f389b6338759
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-05-13 09:02:48 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,9 +3,10 @@ filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prod-offer-eligibil
 filter cloudwatch_log ~ /PromotionValidationRequest received|Customer validated and accepted\. PromotionValidationResponse:/
 
 statsby
-  eligible_count:sum(if(cloudwatch_log ~ /Customer validated and accepted\. PromotionValidationResponse:/, 1, 0)),
-  request_received_count:sum(if(cloudwatch_log ~ /PromotionValidationRequest received/, 1, 0))
+  request_received_count:sum(if(cloudwatch_log ~ /PromotionValidationRequest received/, 1, 0)),
+  eligible_count:sum(if(cloudwatch_log ~ /Customer validated and accepted\. PromotionValidationResponse:/, 1, 0))
 
-make_col eligibility_rate:if(request_received_count = 0, 0.0, 100.0 * eligible_count / request_received_count)
+make_col ineligible_count:request_received_count - eligible_count
+make_col ineligible_rate:if(request_received_count = 0, 0.0, 100.0 * ineligible_count / request_received_count)
 
-pick_col eligibility_rate
+pick_col ineligible_rate
