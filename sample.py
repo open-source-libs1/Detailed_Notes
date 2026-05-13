891521df8e2f9e04d9ba92c9451b330821cfc55b
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-05-13 08:20:57 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,11 @@
+filter asv = "ASVOFFERFULFILLMENTENGINE"
+filter logGroup ~ "/aws/ecs/fargate/BAOFFERFULFILLMENTENGINE/prod-offer-eligibility/"
+filter cloudwatch_log ~ /PromotionValidationRequest received|Customer validated and accepted\. PromotionValidationResponse:/
 
+statsby
+  eligible_count:sum(if(cloudwatch_log ~ /Customer validated and accepted\. PromotionValidationResponse:/, 1, 0)),
+  request_received_count:sum(if(cloudwatch_log ~ /PromotionValidationRequest received/, 1, 0))
+
+make_col eligibility_rate:if(request_received_count = 0, 0.0, 100.0 * eligible_count / request_received_count)
+
+pick_col eligibility_rate
