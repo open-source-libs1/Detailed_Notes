f16b3c29b754445bf28b7276d5cab1882382adc8
open-source-libs1 <sai.pythonengineer@gmail.com>
2026-05-14 07:11:27 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 filter accountId = "578930797225"
-filter label("^Load Balancer") = "prod-offer-eligibility-alb"
+filter label(`^Load Balancer`) = "prod-offer-eligibility-alb"
 filter metric ~ /^(RequestCount|HTTPCode_Target_4XX_Count)$/
 
 timechart 5m,
@@ -8,13 +8,17 @@ timechart 5m,
 
 make_col alb_4xx_error_rate:if(request_count = 0, 0.0, 100.0 * target_4xx_count / request_count)
 
+pick_col timestamp, alb_4xx_error_rate
 
-////////////////////////
+
+///////////////////////
 
 
 filter accountId = "578930797225"
-filter label("^Load Balancer") = "prod-offer-eligibility-alb"
+filter label(`^Load Balancer`) = "prod-offer-eligibility-alb"
 filter metric = "RequestCount"
 
 timechart 5m,
   alb_request_count:sum(value)
+
+pick_col timestamp, alb_request_count
